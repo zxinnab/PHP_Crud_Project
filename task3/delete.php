bdeeacc8bb575feb3a25fe71ced9362a8905edc5
zxinnab <zainab.22cs189@sode-edu.in>
2024-07-20 15:29:42 +0530

    Add files via upload

---
+++ task3/delete.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = $_POST['id'];
+
+    $conn = new mysqli('localhost', 'root', '', 'wshop');
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql="DELETE FROM students
+    WHERE id=$id";
+
+    // SQL statement to delete the entries 
+    if ($conn->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+
+    $conn->close();
+    header("Location: view_details.php");
+}
+?>
